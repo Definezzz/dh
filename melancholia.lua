b09d253777e60137943b15a6e1217ab33c8b391b
definezzz <107827720+Definezzz@users.noreply.github.com>
2024-07-13 00:41:55 +0500

    Update melancholia.lua

--- melancholia.lua
+++ melancholia.lua
@@ -1,2 +1,6 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{
+                Title = ":)",
+                Text = "улыбается - добрый", 
+            })
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Definezzz/dh/main/prikol.lua"))();
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Definezzz/dh/main/melan.lua"))();
